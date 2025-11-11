6492a0e731cf494260ec2793035470867cf0a6e3
Neoakay005 <akayhalil0505@gmail.com>
2025-11-11 16:37:26 +0300

    last stand including abstraction and encapsulation

---
+++ lib/DemoWorks/classes_and_objects.dart
@@ -0,0 +1,27 @@
+void main() {
+  Car bmw = Car(brand: "BMW", motorPower: 350, numberOfDoors: 5);
+  print("Car Brand: ${bmw.brand}");
+  bmw.sound("WuwuwummmmmMMM");
+}
+
+class Car {
+  String brand;
+  int motorPower;
+  int numberOfDoors;
+
+  // constructor
+  Car({
+    required this.brand,
+    required this.motorPower,
+    required this.numberOfDoors,
+  }) {
+    brand = brand;
+    motorPower = motorPower;
+    numberOfDoors = numberOfDoors;
+  }
+
+  // sample method
+  void sound(String soundOfCar) {
+    print(soundOfCar);
+  }
+}
---
+++ lib/DemoWorks/lists.dart
@@ -0,0 +1,20 @@
+void main() {
+  List<String> myList = [
+    "Halil", "Akay", "Mustafa", "Ömer",
+  ];
+  //print out the 4th item
+  print(myList[3]);
+
+  // find index of the word "Akay" in the list
+  print(myList.indexOf("Akay"));
+
+  // adding new item into the list
+  myList.add("Hatice");
+  print(myList);
+
+  // insert a new item into the current list
+  myList.insert(2, "Hamza");
+  print(myList);
+
+  // for more samples https://dart.dev/language/collections
+}
